c807fbe79f53b5d641242790711335a36b0d1795
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-06-05 20:51:58 +0600

    Improve code

--- get_nasa_epic_images.py
+++ get_nasa_epic_images.py
@@ -24,18 +24,14 @@ def get_nasa_epic_image(
     )
     response.raise_for_status()
     photos = response.json()
-    for x in range(number_of_photos+1):
-        try:
-            photo = photos[x]
-            photo_datetime = datetime.strptime(
-                photo["date"],
-                "%Y-%m-%d %H:%M:%S"
-            )
-            photo_name = photo["image"]
-            url = f"https://api.nasa.gov/EPIC/archive/natural/{photo_datetime.year}/{photo_datetime.month:02d}/{photo_datetime.day:02d}/png/{photo_name}.png"
-            download_and_save_image(url, folder, params=params)
-        except IndexError:
-            break
+    for photo in photos[:number_of_photos]:
+        photo_datetime = datetime.strptime(
+            photo["date"],
+            "%Y-%m-%d %H:%M:%S"
+        )
+        photo_name = photo["image"]
+        url = f"https://api.nasa.gov/EPIC/archive/natural/{photo_datetime.year}/{photo_datetime.month:02d}/{photo_datetime.day:02d}/png/{photo_name}.png"
+        download_and_save_image(url, folder, params=params)
 
 
 def main():
