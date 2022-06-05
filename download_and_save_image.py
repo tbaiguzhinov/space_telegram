019c185a06b3ab439a165a7ef050f3d6bdf090a5
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-06-05 20:28:06 +0600

    Add external function to get filename

--- download_and_save_image.py
+++ download_and_save_image.py
@@ -1,3 +1,4 @@
+from distutils import extension
 import os
 from urllib.parse import unquote, urlparse
 
@@ -7,6 +8,14 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
+def get_file_name(url):
+    """Gets url path return filename"""
+    url_path = urlparse(unquote(url)).path
+    _, full_file_name = os.path.split(url_path)
+    filename, extension = os.path.splitext(full_file_name)
+    return filename, extension
+
+
 def download_and_save_image(url, folder, params=None):
     response = requests.get(
         url,
@@ -14,9 +23,7 @@ def download_and_save_image(url, folder, params=None):
         verify=False,
     )
     response.raise_for_status()
-    url_path = urlparse(unquote(url)).path
-    parent, full_file_name = os.path.split(url_path)
-    filename, extension = os.path.splitext(full_file_name)
+    filename, extension = get_file_name(url)
     os.makedirs(folder, exist_ok=True)
     with open(
             os.path.join(folder, f"{filename}{extension}"),
